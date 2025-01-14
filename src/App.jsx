96075f07a76c18811b85e1f30c71629d4483732e
DylanSDev <dylansdiaz.dev@gmail.com>
2025-01-14 00:43:41 -0300

    Se crea la estructura inicial del proyecto con React.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TreePalmIcon as PalmtreeFill } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+    </>
+  );
+}
+
+export default App;
